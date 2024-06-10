2348aa1dc4945cb6f54c82ff2926c310052610d3
2Alexis <144038537+2Alexis@users.noreply.github.com>
2024-06-10 11:44:21 +0200

    ff

--- services/paymentAPI.js
+++ services/paymentAPI.js
@@ -1,44 +1,26 @@
-// // services/paymentAPI.js
+// services/paymentAPI.js
 
-// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
-// exports.processPayment = (paymentDetails, callback) => {
-//     const { cardNumber, cardExpiry, cardCVC, amount } = paymentDetails;
-
-//     stripe.paymentIntents.create({
-//         amount: amount * 100, // Stripe works with the smallest currency unit
-//         currency: 'usd',
-//         payment_method_data: {
-//             type: 'card',
-//             card: {
-//                 number: cardNumber,
-//                 exp_month: parseInt(cardExpiry.split('/')[0]),
-//                 exp_year: parseInt(cardExpiry.split('/')[1]),
-//                 cvc: cardCVC,
-//             },
-//         },
-//         confirm: true,
-//     })
-//     .then(paymentIntent => {
-//         callback(null, paymentIntent);
-//     })
-//     .catch(error => {
-//         callback(error);
-//     });
-// };
-const axios = require('axios');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.processPayment = (paymentDetails, callback) => {
     const { cardNumber, cardExpiry, cardCVC, amount } = paymentDetails;
 
-    axios.post('https://challenge-js.ynovaix.com/payment', {
-        cardNumber,
-        cardExpiry,
-        cardCVC,
-        amount
+    stripe.paymentIntents.create({
+        amount: amount * 100, // Stripe works with the smallest currency unit
+        currency: 'usd',
+        payment_method_data: {
+            type: 'card',
+            card: {
+                number: cardNumber,
+                exp_month: parseInt(cardExpiry.split('/')[0]),
+                exp_year: parseInt(cardExpiry.split('/')[1]),
+                cvc: cardCVC,
+            },
+        },
+        confirm: true,
     })
-    .then(response => {
-        callback(null, response.data);
+    .then(paymentIntent => {
+        callback(null, paymentIntent);
     })
     .catch(error => {
         callback(error);
